47a1fe7c284d5e5eb6944b24ce98b80b46818095
Souameur <mehdi.souameur@amecfw.com>
2025-03-04 17:05:16 +0000

    Entry pages

--- server.js
+++ server.js
@@ -12,6 +12,18 @@ app.get("/", (req,res) => {
     res.render("index");
 })
 
+app.get("/article", (req,res) => {
+    res.render("article");
+})
+
+app.get("/create-entry", (req,res) => {
+    res.render("create-entry");
+})
+
+app.get("/checkout", (req,res) => {
+    res.render("checkout");
+})
+
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`);
 })
